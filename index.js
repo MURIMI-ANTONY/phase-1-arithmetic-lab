cd6760ba7ef7e95a430006139e2274127ae3680e
Murimi Antony <amurimi101@gmail.com>
2023-09-19 11:32:53 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+
+let num3=19
+let num4=5
+let mod=19%5;
+const max = Math.max(2,5,7,20,4,16,8)
